ae9255a468ab8d4e765701ab8d594eef1e0346b4
Do Nam Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2024-12-22 18:36:23 +0700

    Setup redis config with redundancy

---
+++ API/backend/redis/src/main/java/com/teamb/redis/configurations/RedisCacheConfig.java
@@ -0,0 +1,46 @@
+package com.teamb.redis.configurations;
+
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.redis.RedisCacheManager;
+import org.springframework.cache.redis.serializer.RedisSerializationContext;
+import org.springframework.cache.redis.serializer.StringRedisSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
+    }
+
+    @Bean
+    @Override
+    public KeyGenerator keyGenerator() {
+        return (target, method, params) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getSimpleName()).append(":");
+            sb.append(method.getName()).append(":");
+            for (Object param : params) {
+                sb.append(param.toString()).append(",");
+            }
+            return sb.toString();
+        };
+    }
+}
