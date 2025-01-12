fc98e065f25a8c987b88ec2290d27bcbde7e1700
hydl19903 <s3915195@rmit.edu.vn>
2025-01-13 01:33:53 +0700

    Refractor CharityProject model and repository; update references in StatisticService

--- API/backend/charity_projects/src/main/java/com/teamb/charity_projects/repositories/CharityProjectRepository.java
+++ API/backend/charity_projects/src/main/java/com/teamb/charity_projects/repositories/CharityProjectRepository.java
@@ -4,7 +4,7 @@ import com.teamb.charity_projects.models.CharityProject;
 import com.teamb.charity_projects.models.Continent;
 import com.teamb.common.models.ProjectCategoryType;
 import com.teamb.common.models.ProjectStatus;
-
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -29,4 +29,32 @@ public interface CharityProjectRepository extends MongoRepository<CharityProject
     List<CharityProject> findAllByStatusIn(List<ProjectStatus> statuses);
     List<CharityProject> findAllByCharityId(String id);
     List<CharityProject> findAllByCharityIdAndStatusIn(String charityId, List<ProjectStatus> statuses);
+
+    @Query(value = "{ 'charity.id': ?0 }", fields = "{ 'raisedAmount': 1 }")
+     double sumDonationAmountByCharityId(String donorId);
+
+    // Count total number of projects by charityId
+    @Query(value = "{ 'charity.id': ?0 }", count = true)
+    int countProjectsByCharityId(String charityId);
+
+    // Count total number of projects
+    @Query(value = """
+            {
+                continent: {$regex: ?0},
+                country: {$regex: ?1},
+                category: {$regex: ?2}
+            }
+            """, count = true)
+    Double countBy(String continent, String country, String category);
+
+    @Aggregation(pipeline = {
+        "{ '$match': { " +
+            "'continent': { '$regex': ?0, '$options': 'i' }, " +
+            "'country': { '$regex': ?1, '$options': 'i' }, " +
+            "'category': { '$regex': ?2, '$options': 'i' } " +
+        "} }",
+        "{ '$group': { '_id': null, 'totalRaisedAmount': { '$sum': '$raisedAmount' } } }"
+    })
+    Double sumTotalRaisedAmountBy(String continent, String country, String category);
+   
 }
